2d94dd5aaee81317ba2b4212f25a230ebf2d0822
CindyZacarias <cinzacode@gmail.com>
2023-09-30 00:22:49 -0400

    img

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -3,7 +3,7 @@ import React from "react";
 function Testimonio() {
     return (
         <div className="contenedor-testimonio">
-            <img src="" alt="" />
+            <img src={require('../imagenes/testimonio-emma.png')} alt="Emma" className="imagen-testimonio"/>
         </div>
     );
 }
