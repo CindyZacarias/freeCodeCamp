fec613271db405a8ce93cf9b2c6d7e6d61cef040
CindyZacarias <cinzacode@gmail.com>
2023-09-30 21:22:41 -0400

    props

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,14 +1,16 @@
 import React from "react";
 import '../hojas-de-estilo/Testimonio.css';
 
-export function Testimonio() {
+export function Testimonio(props) {
     return (
         <div className="contenedor-testimonio">
-            <img src={require('../imagenes/testimonio-emma.png')} alt="Emma" className="imagen-testimonio"/>
+            <img src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt="Emma" className="imagen-testimonio"/>
             <div className='contenedor-texto-testimonio' >
-                <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-                <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-                <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+                <p className='nombre-testimonio'>
+                    <strong>{props.nombre}</strong> en {props.pais}
+                </p>
+                <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+                <p className='texto-testimonio'>"{props.testimonio}"</p>
             </div>
         </div>
     );
